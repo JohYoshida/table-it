105223147a8523e6f6c7e455a4ef6b2d570e59ee
Joh Yoshida <johyoshida@gmail.com>
2018-05-11 23:17:15 -0700

    Handle special formatting for Slack and Reddit

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ const questions = [
     name: "dimensions",
     message: "How many dimensions?",
     choices: ["2D", "3D"]
+  },
+  {
+    type: "list",
+    name: "formatting",
+    message: "Special formatting?",
+    choices: [
+      {
+        name: "None",
+        value: "none"
+      },
+      {
+        name: "(Slack) Wrap tabled string with three ticks ```",
+        value: "three ticks"
+      },
+      {
+        name: "(Reddit) Start each line with four spaces",
+        value: "four spaces"
+      }
+    ]
   }
 ];
 
@@ -24,14 +43,15 @@ inquirer.prompt(questions).then(answers => {
   // Handle strings with length of 1
   if (answers.string.length === 1) {
     console.log(`You want one character in ${answers.dimensions}? Alright.`);
-    const arr = answers.string.split("")
+    const arr = answers.string.split("");
     console.log(arr[0]);
     return;
   }
 
-  // Handle 2D and 3D 
+  // Initialize array of rows to print
   let rows = [];
 
+  // Handle 2D and 3D
   switch (answers.dimensions) {
     case "2D":
       rows = boxIt(answers.string);
@@ -40,9 +60,32 @@ inquirer.prompt(questions).then(answers => {
       rows = cubeIt(answers.string);
       break;
     default:
-      console.log("How did you even get here?");
+      console.error("Error: Default case in answers.string");
   }
 
+  // Handle special formatting
+  switch (answers.formatting) {
+    case "none":
+      break;
+    case "three ticks":
+      rows.unshift("```");
+      rows.push("```");
+      break;
+    case "four spaces":
+      const formattedRows = [];
+      rows.forEach(row => {
+        let arr = row.split("");
+        arr.unshift("    ");
+        arr = arr.join("");
+        formattedRows.push(arr);
+        rows = formattedRows;
+      });
+      break;
+    default:
+      console.error("Error: Default case in answers.formatting");
+  }
+
+  // Print rows
   rows.forEach(row => {
     console.log(row);
   });
